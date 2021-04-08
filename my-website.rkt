17fdfb6576a87008828d082d1a756c0aac734486
aizichen <1098679804@qq.com>
2021-04-08 11:13:27 +0800

    add not-found.html and set it to the file-not-found-responder

--- my-website.rkt
+++ my-website.rkt
@@ -2,6 +2,7 @@
 
 (require web-server/servlet-env
 	 web-server/dispatch
+	 web-server/configuration/responders
 	 "my-blog.rkt")
 
 
@@ -22,5 +23,8 @@
                #:servlet-path "/"
                #:extra-files-paths (list (build-path "htdocs"))
                #:command-line? #t
+	       #:file-not-found-responder
+	       (gen-file-not-found-responder
+		(build-path "templates/not-found.html"))
                #:ssl? #f
                #:stateless? #f)
