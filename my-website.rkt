8fa5e9a6a7fac7a123763c6cb115e3d525b07805
aizichen <1098679804@qq.com>
2021-04-08 10:18:53 +0800

    website separate & css changed

--- my-website.rkt
+++ my-website.rkt
@@ -1,122 +1,20 @@
 #lang racket
 
-(require web-server/servlet
-         web-server/servlet-env
-         web-server/templates
-         xml
-         "top-tools.rkt"
-         "web-tools.rkt"
-         "model.rkt")
-
-(provide/contract (start (request? . -> . response?)))
-
-;;; VARS
-(define top-style-sheet
-  `(link ([rel "stylesheet"]
-          [href "/top.css"]
-          [type "text/css"])))
-(define bootstrap-style-sheet
-  `(link ([rel "stylesheet"]
-          [href "/bootstrap/bootstrap.min.css"]
-          [type "text/css"])))
-(define bootstrap-js
-  `(script ([type "text/javascript"]
-            [src "/bootstrap/bootstrap.min.js"]
-            [crossorigin "anonymouse"])))
-
-;;; INITIALIZE
-(initialize-blog!)
+(require web-server/servlet-env
+	 web-server/dispatch
+	 "my-blog.rkt")
 
 
-(define (render-post a-blog a-post embed/url)
-  (define (view-post-handler request)
-    (render-post-detail-page a-blog a-post request))
-  (include-template "templates/blog.post-item.html"))
-
-
-;; Entry Servlet For The Server
-(define (start request)
-  (render-blog-page get-new-blog-db request))
-
-
-;; Render Blog Page
-(define (render-blog-page a-blog request)
-  (define (response-generator embed/url)
-    (response/template (include-template "templates/blog.html")))
-  
-  (define (insert-post-handler request)
-    (let* ([bindings (request-bindings request)]
-           [title (extract-binding/single 'title bindings)]
-           [body (extract-binding/single 'body bindings)])
-      (cond
-        [(and (valid-string? title)
-              (valid-string? body))
-         (blog-insert-post! a-blog title body)
-         (render-blog-page a-blog (redirect/get))]
-        [else
-         (occur-error-page "Empty Blog"
-                           "The blog title & body must be specifed."
-                           (lambda (request)
-                             (render-blog-page a-blog (redirect/get)))
-                           request)])))
-  
-  (send/suspend/dispatch response-generator))
-
-
-;; Render Post Details
-(define (render-post-detail-page a-blog a-post request)
-  (define (response-generator embed/url)
-    (response/template (include-template "templates/post-detail.html")))
-  
-  (define (insert-comment-handler request)
-    (let* ([bindings (request-bindings request)]
-           [comment (extract-binding/single 'comment bindings)])
-      (cond
-        [(valid-string? comment)
-         (render-confirm-add-comment-page
-          a-blog
-          comment
-          a-post
-          request)]
-        [else
-         (occur-error-page "Empty Comment"
-                           "You should specify the comment content."
-                           (lambda (request)
-                             (render-post-detail-page a-blog a-post (redirect/get)))
-                           request)])))
-  
-  (define (goback-handler request)
-    (render-blog-page a-blog (redirect/get)))
-  
-  (send/suspend/dispatch response-generator))
-
-
-;; Blog Comment Add Confirm
-(define (render-confirm-add-comment-page a-blog a-comment a-post request)
-  (define (response-generator embed/url)
-    (response/template (include-template "templates/confirm-add-comment.html")))
-  
-  (define (yes-handler request)
-    (post-insert-comment! a-blog a-post a-comment)
-    (render-post-detail-page a-blog a-post (redirect/get)))
-  
-  (define (cancel-handler request)
-    (render-post-detail-page a-blog a-post (redirect/get)))
-  
-  (send/suspend/dispatch response-generator))
-
-;;; Error Ocurred Page
-(define (occur-error-page title message p request)
-  (define (response-generator embed/url)
-    (response/template (include-template "templates/error.html")))
-  
-  (define (back-handler request) (p request))
-  
-  (send/suspend/dispatch response-generator))
+;;; Dispatches
+(define-values (website-dispatch blog-url)
+  (dispatch-rules
+   ;; BLOG
+   [("") (lambda (req)
+	   (blog-entry req))]))
 
 
 ;; Setup The Servlet
-(serve/servlet start
+(serve/servlet website-dispatch
                #:launch-browser? #f
                #:listen-ip #f
                #:port 80
